0cfc30fd802abc5d0b9337ee8b2e4b9fe79bfaa0
dodgeja3 <dodgeja3@msu.edu>
2015-12-11 23:26:42 -0500

    final push

---
+++ FINAL/dodgeja3.github.io/socialmedia.js
@@ -0,0 +1,28 @@
+document.getElementById("homeButton").onclick = function () {
+    location.href = "http://dodgeja3.github.io/FInalSite.html";
+};
+
+document.getElementById("resumeButton").onclick = function () {
+    location.href = "http://dodgeja3.github.io/ResumePage.html";
+};
+
+document.getElementById("gitHubButton").onclick = function () {
+    location.href = "https://github.com/dodgeja3";
+};
+
+document.getElementById("contactButton").onclick = function () {
+    location.href = "http://dodgeja3.github.io/ContactPage.html";
+};
+
+document.getElementById("socialMediaButton").onclick = function () {
+    location.href = "http://dodgeja3.github.io/SocialMediaPage.html";
+};
+
+
+$('.navToggle').on('click', function() {
+	 $('div').toggleClass('show-navbar');
+});
+
+$('.portfolioToggle').on('click', function() {
+	 $('div').toggleClass('show-portfolio');
+});
